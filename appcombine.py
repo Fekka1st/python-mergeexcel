6c8de0425da1a1a87cc998e74f7e3f029c0bd85c
Ferry A.H <59300267+Fekka1st@users.noreply.github.com>
2023-09-27 15:27:13 +0700

    # Add Loading for merge Excel

--- appcombine.py
+++ appcombine.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import glob
+from tqdm import tqdm
 
 file_paths = glob.glob(
     'C:/input/*.xlsx')  # this route for input folder excel
 
 combined_df = pd.DataFrame()
 
-for file_path in file_paths:
+for file_path in tqdm(file_paths, desc="Menggabungkan File Excel"):
     df = pd.read_excel(file_path)
-
     df = df.iloc[:, 1:]
-
     combined_df = pd.concat([combined_df, df], ignore_index=True)
 
 combined_df.to_excel(
